070fdeb6dca6adb76e48f66aa7679cc4d094794f
Alyssa Altimas <ataltimas@gmail.com>
2023-06-26 13:04:08 -0500

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by scholar on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
